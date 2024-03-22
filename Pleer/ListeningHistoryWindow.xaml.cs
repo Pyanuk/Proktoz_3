a6cd9b72f4d1db199799db377dce98bf2d4b47a3
Pyanuk <144242312+Pyanuk@users.noreply.github.com>
2024-03-22 17:10:24 +0300

    Add files via upload

---
+++ Pleer/ListeningHistoryWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+
+namespace Pleer
+{
+    public partial class ListeningHistoryWindow : Window
+    {
+        public ListeningHistoryWindow(List<string> listeningHistory)
+        {
+            InitializeComponent();
+            HistoryListBox.ItemsSource = listeningHistory; 
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
